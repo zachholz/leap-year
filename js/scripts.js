6a42e69c5a9eb251f9374f7fa3421c7bbd7b23ce
Laura Krouse and Zach Holz <zach.a.holz@gmail.com>
2016-06-20 19:07:25 -0700

    Added more specs in the leap year math

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 // ~business logic~
  var leapYear = function(year) {
-   if (year % 100 === 0) {
-     return false;
-   } else if (year % 4 === 0) {
-   return true;
-  } else {
+   if ((year % 4 === 0) && (year % 100 !== 0)) {
+     return true;
+   } else {
    return false;
   }
 };
