00814c9a5c035c80cd1425330cac0592aa529d0d
Laura Krouse and Zach Holz <laura.krouse@pcc.edu>
2016-06-20 19:04:18 -0700

    added new spec, new else if stating 100 in leap year

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // ~business logic~
  var leapYear = function(year) {
-   if (year % 4 === 0) {
+   if (year % 100 === 0) {
+     return false;
+   } else if (year % 4 === 0) {
    return true;
   } else {
    return false;
